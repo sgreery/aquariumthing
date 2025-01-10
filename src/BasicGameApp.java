b633b55ff0ddc09d50b1265ac368f9133c5b84f0
sgreery <samuel_greer27@milton.edu>
2025-01-10 13:50:16 -0500

    m

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,7 +1,7 @@
 //Basic Game Application
 //Version 2
 // Basic Object, Image, Movement
-// Astronaut moves to the right.
+// Celtic moves to the right.
 // Threaded
 
 //K. Chun 8/2018
@@ -38,11 +38,16 @@ public class BasicGameApp implements Runnable {
    public JPanel panel;
    
 	public BufferStrategy bufferStrategy;
-	public Image astroPic;
+	public Image basketballPic;
+	public Image basketballguyPic;
+	public Image hoopPic;
+	public Image backgroundPic;
 
    //Declare the objects used in the program
    //These are things that are made up of more than one variable type
-	private Astronaut astro;
+	private Celtic basketballPlayer;
+	private Celtic basketball;
+	private Celtic hoop;
 
 
    // Main method definition
@@ -62,10 +67,14 @@ public class BasicGameApp implements Runnable {
       setUpGraphics();
        
       //variable and objects
-      //create (construct) the objects needed for the game and load up 
-		astroPic = Toolkit.getDefaultToolkit().getImage("astronaut.png"); //load the picture
-		astro = new Astronaut(10,100);
-
+      //create (construct) the objects needed for the game and load up
+		basketballguyPic = Toolkit.getDefaultToolkit().getImage("aicelticsguy.png");
+		basketballPlayer = new Celtic(100, 100);
+		basketballPic = Toolkit.getDefaultToolkit().getImage("goldbaskteball.png");
+		basketball = new Celtic(500,500);
+		hoopPic = Toolkit.getDefaultToolkit().getImage("aihoop.png");
+		hoop = new Celtic(200, 600);
+		backgroundPic = Toolkit.getDefaultToolkit().getImage("aicourt.png");
 
 	}// BasicGameApp()
 
@@ -92,9 +101,47 @@ public class BasicGameApp implements Runnable {
 	public void moveThings()
 	{
       //calls the move( ) code in the objects
-		astro.move();
-
+		basketballPlayer.move();
+		basketball.move();
+		hoop.move();
+		basketballPlayer.bounce();
+		basketball.bounce();
+		hoop.bounce();
+		collisions();
 	}
+	public void collisions(){
+		if(hoop.rec.intersects(basketballPlayer.rec) && hoop.isCrashing == false){
+			System.out.println("explosion!");
+			hoop.dx = -hoop.dx;
+			hoop.dy = -hoop.dy;
+			basketballPlayer.dx = -basketballPlayer.dx;
+			basketballPlayer.dy = -basketballPlayer.dy;
+			basketballPlayer.width = basketballPlayer.width + 20;
+			basketballPlayer.height = basketballPlayer.height + 20;
+			hoop.dx = hoop.dx * 2;
+			hoop.dy = hoop.dy * 2;
+			hoop.isCrashing = true;
+		}
+		if(!hoop.rec.intersects(basketballPlayer.rec)){
+			hoop.isCrashing = false;
+		}
+		if(basketball.rec.intersects(basketballPlayer.rec) && hoop.isCrashing == false){
+			System.out.println("explosion!");
+			basketball.dx = -basketball.dx;
+			basketball.dy = -basketball.dy;
+			basketballPlayer.dx = -basketballPlayer.dx;
+			basketballPlayer.dy = -basketballPlayer.dy;
+			basketballPlayer.width = basketballPlayer.width + 20;
+			basketballPlayer.height = basketballPlayer.height + 20;
+			basketball.dx = basketball.dx * 2;
+			basketball.dy = basketball.dy * 2;
+			basketball.isCrashing = true;
+		}
+		if(!basketball.rec.intersects(basketballPlayer.rec)){
+			basketball.isCrashing = false;
+		}
+	}
+
 	
    //Pauses or sleeps the computer for the amount specified in milliseconds
    public void pause(int time ){
@@ -140,10 +187,15 @@ public class BasicGameApp implements Runnable {
 	//paints things on the screen using bufferStrategy
 	private void render() {
 		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+
 		g.clearRect(0, 0, WIDTH, HEIGHT);
 
-      //draw the image of the astronaut
-		g.drawImage(astroPic, astro.xpos, astro.ypos, astro.width, astro.height, null);
+      //draws the images of the various celtics related things
+		g.drawImage(backgroundPic, 0, 0, WIDTH, HEIGHT, null);
+		g.drawImage(basketballguyPic, basketballPlayer.xpos, basketballPlayer.ypos, basketballPlayer.width, basketballPlayer.height, null);
+		g.drawImage(basketballPic, basketball.xpos, basketball.ypos, basketball.width, basketball.height, null);
+		g.drawImage(hoopPic, hoop.xpos, hoop.ypos, hoop.width, hoop.height, null);
+
 
 		g.dispose();
 
--- src/Celtic.java
+++ src/Celtic.java
@@ -3,7 +3,7 @@ import java.awt.*;
 /**
  * Created by chales on 11/6/2017.
  */
-public class Astronaut {
+public class Celtic {
 
     //VARIABLE DECLARATION SECTION
     //Here's where you state which variables you are going to use.
@@ -14,7 +14,9 @@ public class Astronaut {
     public int dy;                    //the speed of the hero in the y direction
     public int width;
     public int height;
-    public boolean isAlive;            //a boolean to denote if the hero is alive or dead.
+    public boolean isAlive; //a boolean to denote if the hero is alive or dead.
+    public Rectangle rec;
+    public boolean isCrashing;
 
 
     // METHOD DEFINITION SECTION
@@ -25,14 +27,15 @@ public class Astronaut {
 
     //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
     // if you put in a String, an int and an int the program will use this constructor instead of the one above.
-    public Astronaut(int pXpos, int pYpos) {
+    public Celtic(int pXpos, int pYpos) {
         xpos = pXpos;
         ypos = pYpos;
-        dx =1;
-        dy =0;
-        width = 60;
-        height = 60;
+        dx =5;
+        dy =5;
+        width = 100;
+        height = 100;
         isAlive = true;
+        rec = new Rectangle(xpos, ypos, width, height);
  
     } // constructor
 
@@ -40,7 +43,23 @@ public class Astronaut {
     public void move() {
         xpos = xpos + dx;
         ypos = ypos + dy;
+        rec = new Rectangle(xpos, ypos, width, height);
  
+    }
+    public void bounce(){
+        if(xpos>1000){
+            dx=-dx;
+        }
+        if(ypos>700){
+            dy=-dy;
+        }
+        if(xpos<0){
+            dx=-dx;
+        }
+        if(ypos<0){
+            dy=-dy;
+        }
+        rec = new Rectangle(xpos, ypos, width, height);
     }
 }
 
